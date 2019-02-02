01b92c7fd25a7f18faf2f0bb971a11bef62ecea4
LoliNest <47273609+LoliNest@users.noreply.github.com>
2019-02-02 20:37:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NTQwNzIzMTUxNzIyNzc0NTI4.Dzcohw.d_qjqfbA_MQsMfn4qRhoGHs-M-c);
+client.login(process.env.BOT_TOKEN);
