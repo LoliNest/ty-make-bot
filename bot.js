7b57e3530970a6427a01d8ad3c9ae986f71de00e
LoliNest <47273609+LoliNest@users.noreply.github.com>
2019-02-03 17:11:00 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'Poi') {
-    	message.reply('Poi?');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
